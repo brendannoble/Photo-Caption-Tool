53b35ee78238e120177e2efce8cad5f0ea516d0d
Brendan Noble <brendann755@gmail.com>
2020-08-23 19:34:11 -0600

    Tweak save button

--- src/components/output/OutputContainer.js
+++ src/components/output/OutputContainer.js
@@ -13,8 +13,15 @@ const OutputContainer = () => {
       <h1 id="image-output" className="section-header">Image Output</h1>
       <div className="card  p-5 mb-8">
         { (state.finalImageUrl !== '') ? <SaveButton url={state.finalImageUrl} filename={state.fileName} /> : null }
-        <div className="flex justify-center">
+        <div className="flex flex-col items-center">
           { (state.finalImageUrl === '') ? <OutputPlaceholder/> : <OutputImage url={state.finalImageUrl}/> }
+          { (state.finalImageUrl !== '') ? (
+            <a href="mailto:me@brendannoble.com?subject=PhotoCaptionTool%20Issue">
+              <div className="mt-10 px-4 py-2 font-bold border-2 border-gray-500 inline-block rounded-md hover:bg-yellow-300 hover:border-yellow-600 text-gray-800 transition-colors duration-200">
+                <i className="fas fa-exclamation-triangle mr-1"></i> See an issue? Report it here
+              </div>
+            </a>
+          ) : null }
         </div>
       </div>
     </section>
--- src/components/output/SaveButton.js
+++ src/components/output/SaveButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SaveButton = ({url, filename}) => {
   return (
-    <a className="w-full md:w-auto text-center bg-orange-500 mb-4 text-white rounded-md py-2 px-3 shadow-md inline-block font-bold" href={url} download={`${filename}-captioned.jpg`}>
+    <a className="w-full md:w-auto text-center bg-orange-500 hover:bg-orange-600 mb-4 transition-colors duration-150 text-white rounded-md py-2 px-3 shadow-md inline-block font-bold" href={url} download={`${filename}-captioned.jpg`}>
       <i className="fas fa-file-download mr-2"></i> Save Image
     </a>
   )
