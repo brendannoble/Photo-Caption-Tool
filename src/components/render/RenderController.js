f902a4fbf9899eeedd37cf63be32338bcc1363a5
Brendan Noble <brendann755@gmail.com>
2020-08-25 21:02:36 -0600

    Add service worker for PWA support

--- src/components/render/RenderController.js
+++ src/components/render/RenderController.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import domtoimage from 'dom-to-image';
+import FileSaver, { saveAs } from 'file-saver';
 import PreviewContainer from '../preview/PreviewContainer';
 import OutputContainer from '../output/OutputContainer';
 import RenderButton from './RenderButton';
@@ -20,15 +21,46 @@ const RenderController = () => {
     if (state.isRendering) return;
     dispatch({ type: ACTIONS.TOGGLE_ISRENDERING});
 
-    domtoimage.toJpeg(document.querySelector('#render')).then((dataUrl) => {
-      dispatch({ type: ACTIONS.SET_FINALIMAGEURL, payload: dataUrl});
-      dispatch({ type: ACTIONS.TOGGLE_ISRENDERING});
-      // TODO Implement better scrolling
-      window.location = "#image-output";
-    }).catch((err) => {
-      console.error('Oops, something went wrong!', err);
-      dispatch({ type: ACTIONS.TOGGLE_ISRENDERING});
-    });
+    // Check browser type
+
+    let ua = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+    let browser;
+
+    if (navigator.userAgent.match(/Edge/i) || navigator.userAgent.match(/Trident.*rv[ :]*11\./i)) {
+      browser = "msie";
+    } else {
+      browser = ua[1].toLowerCase();
+    }
+
+    if (browser === 'safari') {
+      domtoimage.toJpeg(document.querySelector('#render')).then((dataUrl) => {
+        dispatch({ type: ACTIONS.SET_FINALIMAGEURL, payload: dataUrl});
+        dispatch({ type: ACTIONS.TOGGLE_ISRENDERING});
+        // TODO Implement better scrolling
+        window.location = "#image-output";
+      }).catch(err => {
+        alert('An error has occurred');
+        console.error('Oops, something went wrong!', err);
+        dispatch({ type: ACTIONS.TOGGLE_ISRENDERING});
+      });
+    } else {
+      domtoimage.toSvg(document.querySelector('#render')).then((dataUrl) => {
+
+        let blob;
+
+        fetch(dataUrl)
+        .then(res => blob = res.blob())
+        .then(FileSaver.saveAs(blob))
+
+        dispatch({ type: ACTIONS.SET_FINALIMAGEURL, payload: dataUrl});
+        dispatch({ type: ACTIONS.TOGGLE_ISRENDERING});
+        window.location = "#image-output";
+      }).catch(err => {
+        alert('An error has occurred');
+        console.error('Oops, something went wrong!', err);
+        dispatch({ type: ACTIONS.TOGGLE_ISRENDERING});
+      });
+    }
   }
 
   return (
